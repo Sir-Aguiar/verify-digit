8777d0e529ceebf7568d3f50383bf173adaab773
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2023-05-31 23:18:01 -0400

    Final version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
   Regras:
   - A sequência deve ter 7 caracteres (Entrada);
@@ -8,17 +7,33 @@
   - O número decimal obtido será o dígito verificador.
 */
 
-const getFirstBin = (decimal) => decimal % 2;
+const binToDec = (bin) => parseInt(bin, 2).toString(10);
+const decToBin = (dec) => parseInt(dec, 10).toString(2);
 
+const getLastBit = (decimal) => decimal % 2;
 const getBinarySequence = (seq) => {
 	const sequence = seq.split("");
-	const binaries = sequence.map((dec) => getFirstBin(Number(dec))).join("");
+	// Formando um array com o último bit de cada termo da sequência recebida no parametro "seq"
+	const binaries = sequence.map((dec) => getLastBit(Number(dec))).join("");
 	return binaries;
 };
-
-const binToDec = (bin) => parseInt(bin, 2).toString(10);
-
 const getHashCode = (seq) => binToDec(getBinarySequence(seq));
+
+const generateSequenceByHash = (hash) => {
+	const hashBinaries = decToBin(hash).padStart(7, 0).split("");
+	let possibleSequence = "";
+
+	// Para cada bit presente no array, gero um algarismo aleatório de igual paridade (0 = par, 1 = ímpar)
+	hashBinaries.forEach((digit) => {
+		let randomNum = Math.ceil(Math.random() * 9);
+		while (randomNum % 2 !== Number(digit)) {
+			randomNum = Math.ceil(Math.random() * 9);
+		}
+		possibleSequence += randomNum;
+	});
+
+	return `${possibleSequence}-${hash}`;
+};
 
 const verifySequence = (sequence) => {
 	const [numericSequence, hashCode] = sequence.split("-");
